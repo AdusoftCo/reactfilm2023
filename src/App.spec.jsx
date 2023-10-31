a120b94ec9024a77b01e0e45e447cf0c57986853
EmaXime <janere_645@hotmail.com>
2023-10-31 17:02:50 -0300

    fix

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -6,9 +6,7 @@ import { describe, expect, it } from 'vitest';
 describe('App', () => {
     it('should render component', () => {
         const component = render(<App />);
-
-        const title = component.getByText("ESTO NO ESTA");
-        expect(title).toBe("asdljdad");
-        //expect(component).toBeDefined();
+        
+        expect(component).toBeDefined();
     });
 });
